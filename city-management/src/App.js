2d864216894627c1ec5e50839cabd0a619d43924
yzheng21 <zhengyufan94@gmail.com>
2020-01-12 19:37:38 -0800

    modularized antd

--- city-management/src/App.js
+++ city-management/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button>Click</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
